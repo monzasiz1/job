544a94c28f413dbdec0c99a9b91a61b2f2fe4bb4
monzasiz1 <rene-schroers@gmx.de>
2026-04-13 23:25:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Syne:wght@600;700;800&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
